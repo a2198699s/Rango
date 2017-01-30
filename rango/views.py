ee8b053e4aff6c7fc99132a03f553e350c560d58
Andrew Smith <2198699s@student.gla.ac.uk>
2017-01-30 15:08:49 +0000

    About functionality and up to end of chapter 5

--- rango/views.py
+++ rango/views.py
@@ -12,6 +12,7 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 	
 def about(request):
-    context_dict = {'boldmessage': "This tutorial was put together by Andrew Smith"}
-    return render(request, 'rango/about.html', context=context_dict)
+	print (request.method)
+	print (request.user)
+	return render(request, 'rango/about.html',{})
 
