50a71f0ec5b9b6a1e0bdaae7919dd203d2f7ca14
Andrew Smith <2198699s@student.gla.ac.uk>
2017-02-06 09:29:31 +0000

    Categories shown in order of likes

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,9 @@ from rango.models import Category
 from django.http import HttpResponse
 
 def index(request):
-	context_dict = {'boldmessage': "Andy waz ere"}
-	category_list = Category.object.order_by("-likes")[:5]
+	#context_dict = {'boldmessage': "Andy waz ere"}
+	category_list = Category.objects.order_by("-likes")[:5]
+	context_dict = {'categories': category_list}
 	return render(request, 'rango/index.html', context=context_dict)
 	
 def about(request):
