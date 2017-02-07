bdae6ebb82a2e862eedaf1f54af5c30e4c6f266c
Andrew Smith <2198699s@student.gla.ac.uk>
2017-02-07 10:44:46 +0000

    Pages and categories populated and cats showing links

--- populate_rango.py
+++ populate_rango.py
@@ -1,39 +1,33 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-'tango_with_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+
+
 import django
 django.setup()
-from rango.models import Category, Page
-def populate():
+from rango.models import Category,Page
+
 # First, we will create lists of dictionaries containing the pages
 # we want to add into each category.
 # Then we will create a dictionary of dictionaries for our categories.
 # This might seem a little bit confusing, but it allows us to iterate
 # through each data structure, and add the data to our models.
+def populate():
 	python_pages = [
-	{"title": "Official Python Tutorial","url":"http://docs.python.org/2/tutorial/"},
-	{"title":"How to Think like a Computer Scientist",
-	"url":"http://www.greenteapress.com/thinkpython/"},
-	{"title":"Learn Python in 10 Minutes",
-	"url":"http://www.korokithakis.net/tutorials/python/"} ]
-
+		{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 32},
+		{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
+	    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views": 8},  ]
 	django_pages = [
-	{"title":"Official Django Tutorial",
-	"url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-	{"title":"Django Rocks",
-	"url":"http://www.djangorocks.com/"},
-	{"title":"How to Tango with Django",
-	"url":"http://www.tangowithdjango.com/"} ]
-
+		{"title":"Official Django Tutorial",
+	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
+		{"title":"Django Rocks",
+	        "url":"http://www.djangorocks.com/" , "views": 16},
+		{ "title":"How to Tango with Django",
+	        "url":"http://www.tangowithdjango.com/", "views": 8 } ]
 	other_pages = [
-	{"title":"Bottle",
-	"url":"http://bottlepy.org/docs/dev/"},
-	{"title":"Flask",
-	"url":"http://flask.pocoo.org"} ]
-
-	cats = {"Python": {"pages": python_pages},
-	"Django": {"pages": django_pages},
-	"Other Frameworks": {"pages": other_pages} }
+		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views": 32},
+		{ "title":"Flask", "url":"http://flask.pocoo.org", "views": 16} ]
+	cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},"Django": {"pages": django_pages, "views": 64, "likes": 32},"Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},"Pascal": {"pages": [], "views": 32, "likes": 16},"Perl": {"pages": [], "views": 32, "likes": 16},"Php": {"pages": [], "views": 32, "likes": 16},"Prolog": {"pages": [], "views": 32, "likes": 16},"Programming": {"pages": [], "views": 32, "likes": 16},}
 
  # If you want to add more catergories or pages, www.tangowithdjango.com Models and Databases 56
  # add them to the dictionaries above.
@@ -45,9 +39,9 @@ def populate():
  # for more information about how to iterate over a dictionary properly.
  # before change : for cat, cat_data in cats.items():
 	for cat, cat_data in cats.items():
-		c = add_cat(cat,128,64)
-	for p in cat_data["pages"]:
-		add_page(c, p["title"], p["url"])
+		c = add_cat(cat, cat_data["views"], cat_data["likes"])
+		for p in cat_data["pages"]:
+			add_page(c, p["title"], p["url"], p["views"])
 
  # Print out the categories we have added.
 	for c in Category.objects.all():
@@ -61,10 +55,10 @@ def add_page(cat, title, url, views=0):
 	p.save()
 	return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
 	c = Category.objects.get_or_create(name=name)[0]
-	c = Category.objects.get_or_create(likes=likes)[0]
-	c = Category.objects.get_or_create(views=views)[0]
+	c.views=views
+	c.likes=likes
 	c.save()
 	return c
 
--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,8 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    url(r'^$', views.index, name='index'),url(r'about', views.about, name = 'about'),
+    url(r'^$', views.index, name='index'),
+	url(r'about', views.about, name = 'about'),
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+	views.show_category, name='show_category'),
 	]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
