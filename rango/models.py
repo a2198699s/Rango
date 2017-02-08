5817f42fd81007608d2d2ddb143d586bc3f000c4
Andrew Smith <2198699s@student.gla.ac.uk>
2017-02-08 12:43:16 +0000

    Ex. 8 before excersises

--- rango/models.py
+++ rango/models.py
@@ -6,10 +6,12 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
-	views = models.IntegerField(default = 0)
+	views = models.IntegerField(default=0)
 	likes = models.IntegerField(default = 0)
 	slug = models.SlugField(unique=True)
 	def save(self, *args, **kwargs):
+		if self.views < 0:
+			self.views = 0
 		self.slug = slugify(self.name)
 		super(Category, self).save(*args, **kwargs)
 
