6a9e7e06101dbc963091b98ea29050fb5fba2384
Andrew Smith <2198699s@student.gla.ac.uk>
2017-02-10 08:41:55 +0000

    Up to 9.7

--- rango/urls.py
+++ rango/urls.py
@@ -7,8 +7,17 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 	url(r'about/$', views.about, name='about'),
 	url(r'^add_category/$', views.add_category, name='add_category'),
+	
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
-	views.add_page, name='add_page'),
+		views.add_page,
+		name='add_page'),
+		
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
-	views.show_category, name='show_category'),
+		views.show_category,
+		name='show_category'),
+		
+	url(r'^register/$',
+		views.register,
+		name='register'), 
+
 	]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
